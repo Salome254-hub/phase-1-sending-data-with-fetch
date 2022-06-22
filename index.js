a4b9b06eb13a0609bf0f9c1a114b2191ffbf8287
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-22 09:41:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name , email) {
+    const userInfo = {name, email}
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(res => res.json()
+      )
+      .then(user => {
+          document.body.innerHTML = user.id
+      })
+      .catch(err => {document.body.innerHTML = err.message});
+}
